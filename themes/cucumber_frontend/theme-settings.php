9a5b3baeb1868c874e3793687e8d5b70a2489206
Rajab Natshah <rajabn@gmail.com>
2017-06-03 01:26:39 +0300

    Issue #2883533: Bettered images for [Cucumber Install, Cucumber Front-End, and Cucumber Back-End] Themes

--- themes/cucumber_frontend/theme-settings.php
+++ themes/cucumber_frontend/theme-settings.php
@@ -5,10 +5,13 @@
  * Provides theme settings for cucumber_frontend.
  */
 
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Implements hook_form_system_theme_settings_alter().
  */
-function cucumber_frontend_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id = NULL) {
+function cucumber_frontend_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
+
   // General "alters" use a form id. Settings should not be set here. The only
   // thing useful about this is if you need to alter the form for the running
   // theme and *not* the theme setting.
