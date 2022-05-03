50d6bcf07311250f3a6bcda6a92fb782bc7463e0
TasnimNatsheh <t.natshah@gmail.com>
2022-05-03 20:53:46 +0300

    Issue #3277241 by Tasneem Natshah: Fix cucumber-js: not found for the Cucumber automated testing

--- tests/step-definitions/cucumber.js
+++ tests/step-definitions/cucumber.js
@@ -1,13 +1,15 @@
-const {Given,When, Then, Before} = require('@cucumber/cucumber');
+const {Given} = require('@cucumber/cucumber');
+const {When, Before} = require('@cucumber/cucumber');
+const {Then} = require('@cucumber/cucumber');
 
 Given(/^I go to "([^"]*)?"$/, function(url) {
   return browser.url(url);
 });
 
-Then(/^I should( not)* see "([^"]*)?"$/, function(negativeCase, text) {
+Then(/^I should( not)* see "([^"]*)?"$/, function(negativeCase, expectedText) {
   if (negativeCase) {
-    return browser.assert.not.textContains(text);
+    return browser.assert.not.textContains('body', expectedText);
   }
   
-  return browser.assert.textContains(text);
+  return browser.assert.textContains('body', expectedText);
 });
