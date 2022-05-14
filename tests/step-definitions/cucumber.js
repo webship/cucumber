0f0e416f0ab384b178e878d5d1e9d0fb5cc72fc8
TasnimNatsheh <t.natshah@gmail.com>
2022-05-14 16:17:13 +0300

    Issue #3280468: Add bash command to  add testing users for Cucumber

--- tests/step-definitions/cucumber.js
+++ tests/step-definitions/cucumber.js
@@ -3,7 +3,8 @@ const {When, Before} = require('@cucumber/cucumber');
 const {Then} = require('@cucumber/cucumber');
 
 Given(/^I go to "([^"]*)?"$/, function(url) {
-  return browser.url(url);
+  return browser.url(url)
+                .waitForElementVisible('body', 1000);
 });
 
 Then(/^I should( not)* see "([^"]*)?"$/, function(negativeCase, text) {
